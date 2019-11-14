f32341db745d4a711c8d890412622a4601b26599
Mario Geiger <geiger.mario@gmail.com>
2019-11-14 17:59:59 +0100

    nicer code

--- parse.py
+++ parse.py
@@ -3,114 +3,140 @@ import json
 import pickle
 from html.parser import HTMLParser
 
+import tqdm
 from IPython.core.display import HTML, display
-from tqdm import tqdm as tqdm
 
 
-class ISA_HTMLParser(HTMLParser):
+class Element:
+    def __init__(self, tag, parent):
+        self.tag = tag
+        self.parent = parent
+        self.content = []
+        self.data = []
+
+    def html(self, ind=0):
+        if self.content:
+            t = '{}<{}>\n'.format(' ' * ind, self.tag)
+            for d in self.data:
+                t += '{}    {}\n'.format(' ' * ind, d)
+            for c in self.content:
+                t += '{}\n'.format(c.html(ind + 4))
+            t += '{}</{}>'.format(' ' * ind, self.tag)
+        else:
+            data = self.data
+            if len(data) == 0:
+                data = ''
+            if len(data) == 1:
+                data = data[0]
+            t = '{}<{}>{}</{}>'.format(' ' * ind, self.tag, data, self.tag)
+        return t
+
+    def __repr__(self):
+        return self.html()
+
+    def hastag(self, tag):
+        for c in self.content:
+            if c.tag == tag or c.hastag(tag):
+                return True
+        return False
+
+    def __getitem__(self, i):
+        return self.content[i]
+
+    def __len__(self):
+        return len(self.content)
+
+    def __iter__(self):
+        return iter(self.content)
+
+
+class HTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
-        self.data = []
-        self.tables = []
-        self.tr = None
-        self.entry = None
+        self.root = Element("html", None)
+        self.current = self.root
+        self.tags = ['table', 'tr', 'td', 'th']
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'table':
-            self.tables.append([])  # start saving a table (you can have a table into another table)
-        if tag == 'tr':
-            self.tr = []  # start saving a line (you cannot have a line into another line)
-        if tag in ['td', 'th']:
-            self.entry = []  # start saving an entry (you cannot have an entry into another entry)
+        if tag in self.tags:
+            el = Element(tag, self.current)
+            self.current.content.append(el)
+            self.current = el
 
     def handle_endtag(self, tag):
-        if tag == 'table' and self.tables:
-            table = self.tables.pop()
-            if table:  # don't save empty tables
-                if self.tables:
-                    self.tables[-1].append(table)
-                else:  # root table
-                    self.data.append(table)
-
-        if tag == 'tr' and type(self.tr) == list:
-            if self.tables:
-                if self.tr:  # don't save empty lines
-                    self.tables[-1].append(self.tr)
-                self.tr = None
-            else:
-                print("tr outside a table: {}".format(self.tr))
-
-        if tag in ['td', 'th'] and self.entry:
-            if type(self.tr) == list:
-                self.tr.append("\n".join(self.entry))
-                self.entry = None
-            else:
-                print("td/th outside a tr: {}".format(self.td))
+        if tag in self.tags:
+            assert self.current.tag == tag
+            self.current = self.current.parent
 
     def handle_data(self, data):
-        if type(self.entry) is list:
-            self.entry.append(data.strip())
+        if self.current.tag in self.tags:
+            data = data.strip()
+            if data:
+                self.current.data.append(data)
 
 
-def parse(raw):
-    parser = ISA_HTMLParser()
-    parser.feed(raw)
-
-    if len(parser.data) == 0:
+def parse(html):
+    parser = HTMLParser()
+    parser.feed(html)
+    if len(parser.root) == 0:
         return []
+    assert len(parser.root) == 1, parser.root
+    main = parser.root.content[0]
 
-    main = parser.data[0]
-
-    if len(main[0]) != 2:
-        return []
-
-    course, teacher = main[0]
-    course = course.strip()
-
-    if 'Enseignant' in teacher:
-        teacher = teacher.split('Enseignant-e-(s):')[1]
-        if 'Assistant-e-(s):' in teacher:
-            teacher, assistant = teacher.split("Assistant-e-(s):")
-        else:
-            assistant = ""
-        teacher = teacher.strip()
-        assistant = assistant.strip()
-    else:
-        teacher = teacher.strip()
-        assistant = ""
+    course = None
+    teacher = None
+    assistant = None
+    label = None
 
     students = []
 
-    def fmt(x):
-        if len(x) == 3:
-            return x
-        if len(x) < 3:
-            return x + [""] * (3 - len(x))
-        assert False, x
+    for line in main:
+        assert line.tag == 'tr'
 
-    def valid(x):
-        if type(x[0]) is list:
-            return False
-        return True
+        if line.hastag('th'):
+            assert len(line) == 2
 
-    i = 1
-    while i < len(main):
-        if len(main[i]) == 1:
-            label = main[i][0]
-            i += 1
+            assert len(line[0].data) == 1
+            course = line[0].data[0]
 
-            if i >= len(main):
-                break
+            text = line[1].data
+            if len(text) == 3:
+                text = text[1:]
 
-            if 'ét' in main[i][0]:
-                i += 1
+            assert len(text) <= 2, line
+            if len(text) == 0:
+                teacher, assistant = "", ""
+            if len(text) == 1:
+                teacher, assistant = text[0], ""
+            if len(text) == 2:
+                teacher, assistant = text
 
-                students += [fmt(x) + [course, teacher, assistant, label] for x in main[i] if valid(x)]
-                i += 1
-            else:
-                i += 0
-        else:
-            i += 1
+            if teacher.startswith('Enseignant-e-(s): '):
+                teacher = teacher[len('Enseignant-e-(s): '):]
+            if assistant.startswith('Assistant-e-(s): '):
+                assistant = assistant[len('Assistant-e-(s): '):]
+            continue
+
+        if line.hastag('table'):
+            if label is not None:
+                assert course is not None and teacher is not None and assistant is not None
+                assert len(line) == 1
+                assert len(line[0]) == 1
+                table = line[0][0]
+                assert table.tag == 'table'
+                for st in table:
+                    assert st.tag == 'tr', table
+                    assert len(st) == 4, table
+                    st = [" ".join(x.data) for x in st] + [label, course, teacher, assistant]
+                    students.append(st)
+                label = None
+            continue
+
+        if line.hastag('td') and line[0].data:
+            assert len(line) == 1
+            if not 'ét.)' in line[0].data[0]:
+                label = line[0].data[0]
+            continue
 
     return students
 
@@ -118,24 +144,15 @@ def parse(raw):
 with open('raw.pkl', 'rb') as f:
     periods = pickle.load(f)
 
-stop = False
+with open('raw.pkl', 'rb') as f:
+    periods = pickle.load(f)
 
-for (year, season), xs in tqdm(periods.items()):
+for (year, season), htmls in tqdm.tqdm(periods.items()):
     students = []
-    for html in tqdm(xs):
-        try:
-            students += parse(html)
-        except:
-            print(html)
-            display(HTML(html))
-            stop = True
-
-        if stop:
-            break
-    if stop:
-        break
+    for html in tqdm.tqdm(htmls):
+        students += parse(html)
 
     with open("data{}{}.csv".format(year, season), 'w') as f:
         writer = csv.writer(f)
-        for x in students:
-            writer.writerow(x)
+        for st in students:
+            writer.writerow(st)
